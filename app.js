986072f9f5b7dd86d173cb8871d1f1d42a517083
iorimatt <m.iori.franco@gmail.com>
2021-12-19 15:39:22 +0000

    storage CRUD in json file

--- app.js
+++ app.js
@@ -2,27 +2,15 @@ const express = require('express');
 const res = require('express/lib/response');
 const app = express()
 const racelist = require('./routes/racelist')
-
-
-
 app.use(express.json())
 app.use('/racelist', racelist)
-
-
-
-
 app.listen(3000, () => {
     console.log('Server running on 3000')
 });
-
-
-
-
 app.get('/', (req, res) => {
     res.send(200)
 }
 );
-
 app.get('/races/:name', (req, res) => {
    
     const {name} = req.params
--- routes/racelist.js
+++ routes/racelist.js
@@ -1,61 +1,85 @@
 const { Router } = require("express");
-
-
+const fs = require('fs');
+const { send } = require("process");
 const router = Router()
-
 const raceList = [];
-
-
 router.get('/', (req, res) => {
-
-
     res.send(raceList)
-
-
 })
-
+router.post('/newList', (req, res) => {
+ 
+  
+    const data = JSON.stringify(raceList)
+    fs.writeFile('race-list.json', data, { flag: "wx" }, (err)=>{
+        if (err) {
+            if(err.code === 'EEXIST'){
+                res.send('Only one list is allowed') 
+            }
+           
+        }
+        res.send('list created')
+    })
+})
 router.post('/createRace', (req, res) => {
-
-    const race = {
-
-        id: raceList.length + 1, 
-        name: req.body.name,
-        location: req.body.location
-    }
-    raceList.push({ race });
-    res.send('Race Created')
+    fs.readFile('race-list.json', 'utf-8', (err, data)=>{
+        if(err){console.log(err) }
+        const raceList = JSON.parse(data.toString())
+        const race = {
+            name: req.body.name,
+            location: req.body.location
+        }
+        
+        raceList.push({ race });
+        data = JSON.stringify(raceList)
+        fs.writeFile('race-list.json', data, (err) => {
+            if (err) { console.log(err)  }
+            res.send('Race Created')
+    
+        })
+    })
+    
+    
+   
+   
 });
-
-router.post('/deleteRace/id=:id', (req, res)=> {
-
-    const { id } = req.params
-    const select = id - 1
-    raceList.splice(select)
-    res.send('Race Deleted')
-
+router.post('/deleteRace/id=:id', (req, res) => {
+    fs.readFile('race-list.json', 'utf-8', (err, data) => {
+        if (err) { console.log(err)  }
+        const raceList = JSON.parse(data)
+        const { id } = req.params
+        raceList.splice(id, 1)
+        data = JSON.stringify(raceList)
+        fs.writeFile('race-list.json', data, (err) => {
+            if (err) {
+                console.log(err);
+            }
+            res.send('Race Deleted')
+        })
+    })
 },
-
-
-
-router.put('/updateRace/id=:id', (req, res) => {
-
-
-            const {id} = req.params
-            const select = id - 1
-            const update = raceList[select].race
+    router.put('/updateRace/id=:id', (req, res) => {
+            
+          fs.readFile('race-list.json', 'utf-8', (err, data) => {
+            if (err) { throw err }
+    
+            const raceList = JSON.parse(data)
+            const { id } = req.params
+            const update = raceList[id].race
             update.name = req.body.name
             update.location = req.body.location
-
-            res.send('Race Updated')
-            
-
-
-})
+            data = JSON.stringify(raceList)
+    
+            fs.writeFile('race-list.json', data, (err) => {
+                if (err) {
+                    throw err;
+                }
+                res.send('Race Deleted')
+    
+            })
+    
+    
+        })
+    res.send('Race Updated')
+    })
 )
-
-
-
-
 module.exports = router;
-
-
